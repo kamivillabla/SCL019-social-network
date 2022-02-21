a260376f80747f74d983480a058a019fd4eb431e
JavieraMunita <95220695+JavieraMunita@users.noreply.github.com>
2022-02-21 19:12:44 -0300

    Verificación correo electrónico

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,8 +8,9 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
   signInWithEmailAndPassword,
-} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
-import { app } from './config-firebase.js';
+  sendEmailVerification,
+} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { app } from "./config-firebase.js";
 
 const auth = getAuth();
 
@@ -20,7 +21,11 @@ export const newRegister = (email, password) => {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      window.location.hash = '#/login';
+      verificar();
+      alert(
+        "Se ha enviado un correo electrónico de verificación. Por favor revisa tu bandeja de entrada."
+      );
+      window.location.hash = "#/login";
       return user;
     })
     .catch((error) => {
@@ -31,16 +36,14 @@ export const newRegister = (email, password) => {
       console.log(errorCode);
       // return errorCode + errorMessage;
       // Mensaje correo en uso
-      if (errorCode == 'auth/email-already-in-use') {
-        const emailInUse = document.getElementById(
-          'registerEmailInUse',
-        );
-        emailInUse.style.display = 'block';
+      if (errorCode == "auth/email-already-in-use") {
+        const emailInUse = document.getElementById("registerEmailInUse");
+        emailInUse.style.display = "block";
       }
       // Mensaje contraseña demasiado debil (Menos de 6 caracteres)
-      if (errorCode == 'auth/weak-password') {
-        const weakPassword = document.getElementById('registerWeakPassword');
-        weakPassword.style.display = 'block';
+      if (errorCode == "auth/weak-password") {
+        const weakPassword = document.getElementById("registerWeakPassword");
+        weakPassword.style.display = "block";
       }
     });
   return createUserWithEmailAndPassword;
@@ -57,7 +60,7 @@ export const loginGoogle = () => {
       // The signed-in user info.
       const user = result.user;
       // ...
-      window.location.hash = '#/home';
+      window.location.hash = "#/home";
     })
     .catch((error) => {
       // Handle Errors here.
@@ -78,23 +81,31 @@ export const loginEmail = (email, password) => {
       // Signed in
       const user = userCredential.user;
       // ...
-      alert('ingresaste');
+      alert("ingresaste");
       // LLeva al home
-      window.location.hash = '#/home';
+      window.location.hash = "#/home";
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorCode + errorMessage);
-      if (errorCode == 'auth/wrong-password') {
+      if (errorCode == "auth/wrong-password") {
         const errorContraseña = document.getElementById(
-          'loginContrañaInvalida',
+          "loginContrañaInvalida"
         );
-        errorContraseña.style.display = 'block';
+        errorContraseña.style.display = "block";
       }
-      if (errorCode == 'auth/user-not-found') {
-        const errorEmail = document.getElementById('loginEmailInvalido');
-        errorEmail.style.display = 'block';
+      if (errorCode == "auth/user-not-found") {
+        const errorEmail = document.getElementById("loginEmailInvalido");
+        errorEmail.style.display = "block";
       }
     });
+};
+
+const verificar = () => {
+  sendEmailVerification(auth.currentUser).then(() => {
+    console.log("Mail enviado");
+    // Email verification sent!
+    // ...
+  });
 };
