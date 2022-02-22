35617ae1d210af104bbe3a45e57d5a744d93b598
Kamila <kvillablancabaez@gmail.com>
2022-02-21 21:56:10 -0300

    arreglo pagina

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// Este es el punto de entrada de tu aplicacion
 import { myFunction } from './lib/index.js';
 
 import { routes } from './lib/routes.js';
@@ -17,8 +16,10 @@ const init = () => {
   // observador();
 };
 
+init();
+
 // Al terminar de cargar la página(Event Load) se ejecuta la función INIT.
-window.addEventListener('load', init);
+// window.addEventListener('load', init);
 
 /*  El evento hashchange es ejecutado cuando el fragmento identificador de la URL ha cambiado. */
 window.addEventListener('hashchange', () => {
