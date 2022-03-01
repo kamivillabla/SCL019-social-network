095a5b3568f808475204549bd74ce384be6dcd1c
JavieraMunita <95220695+JavieraMunita@users.noreply.github.com>
2022-03-01 11:09:23 -0300

    Inputs y spams usuario y edad

--- src/pages/registro.js
+++ src/pages/registro.js
@@ -1,10 +1,10 @@
-import { footer } from '../component/footer.js';
-import { newRegister, loginGoogle } from '../lib/firebase.js';
+import { footer } from "../component/footer.js";
+import { newRegister, loginGoogle } from "../lib/firebase.js";
 
 export const register = () => {
   // Acá ira lo que desplegaremos.
-  const registerContainer = document.createElement('div');
-  registerContainer.classList.add('containerRoot__grid');
+  const registerContainer = document.createElement("div");
+  registerContainer.classList.add("containerRoot__grid");
   registerContainer.innerHTML = `
   <div class="logo gridItem">
       <img src="assets/img/logoMartini.png" alt="Imagen logo alcohdemia" class="logoImg">
@@ -13,13 +13,18 @@ export const register = () => {
     <h1>BUENA MUCHACHA</h1>
       <form id="loginForm" class="form gridItem">
         <p>Ingresa tus datos</p>
+        
+        <input type="text" id="userName" placeholder="Nombre de Usuarie">
+        <spam class="displayNone" id="invalidUserName">Nombre de usuarie no disponible</spam>
+        <spam class="displayNone" id="emptyUserName">Ingresa un nombre de usuarie</spam>
+        <input type="date" id="age" placeholder="Fecha de nacimiento" required>
+        <spam class="displayNone" id="invalidAge">Lo siento, estás muy chiquite</spam>
+        <spam class="displayNone" id="emptyAge">Ingresa tu fecha de nacimiento</spam>
         <input type="text" id="registerEmail" placeholder="Correo Electrónico" required>
         <spam class="displayNone" id="registerEmailInUse">Este correo ya se encuentra en uso</spam>
         <spam class="displayNone" id="missinEmail">Ingresa un correo</spam>
         <spam class="displayNone" id="loginEmailNull">Este correo no es válido</spam>
-        <input type="text" id="userName" placeholder="Usuario">
         <input type="password" id="registerPassword" placeholder="Contraseña" required>
-        <input type="text" id="age" placeholder="Fecha de nacimiento" required>
         <spam class="displayNone" id="registerWeakPassword">La contraseña debe tener al menos 6 caracteres</spam>
         <spam class="displayNone" id="missinPassword">Ingresa una contraseña</spam>
         <button id="registro" class="register__buttonLogin" type="submit">Registro</button>
@@ -36,20 +41,23 @@ export const register = () => {
 
   // Button Login google
   registerContainer
-    .querySelector('#registerButton')
-    .addEventListener('click', () => {
+    .querySelector("#registerButton")
+    .addEventListener("click", () => {
       loginGoogle();
     });
 
   // Button register
-  registerContainer.querySelector('#registro').addEventListener('click', (e) => {
-    e.preventDefault();
-    const email = registerContainer.querySelector('#registerEmail').value;
-    const password = registerContainer.querySelector('#registerPassword').value;
-    const userName = registerContainer.querySelector('#userName').value;
+  registerContainer
+    .querySelector("#registro")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      const email = registerContainer.querySelector("#registerEmail").value;
+      const password =
+        registerContainer.querySelector("#registerPassword").value;
+      const userName = registerContainer.querySelector("#userName").value;
 
-    newRegister(email, password, userName);
-  });
+      newRegister(email, password, userName);
+    });
 
   registerContainer.appendChild(footer());
   return registerContainer;
