58d54f3c45e4b7cc55c46bd88884f1f0191de81b
Kamila <kvillablancabaez@gmail.com>
2022-03-03 13:16:41 -0300

    firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -15,46 +15,45 @@ import {
   sendEmailVerification,
   onAuthStateChanged,
   signOut,
-} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
 
 import {
   getFirestore,
   collection,
   addDoc,
-} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
 
-import { app } from "./config-firebase.js";
-import { routes } from "./routes.js";
+import { app } from './config-firebase.js';
+import { routes } from './routes.js';
 
 export const auth = getAuth();
 export const db = getFirestore();
 
-//Creando colección de datos (noticia en proceso)
+// Creando colección de datos (noticia en proceso)
 const userData = async (userId, userName, age) => {
   try {
-    const docRef = await addDoc(collection(db, "users"), {
+    const docRef = await addDoc(collection(db, 'users'), {
       id: userId,
       name: userName,
       age,
     });
-    console.log("Document written with ID: ", docRef.id);
+    console.log('Document written with ID: ', docRef.id);
   } catch (e) {
-    console.error("Error adding document: ", e);
+    console.error('Error adding document: ', e);
   }
 };
 
-//Colección leer datos para despues publicar
-export const publicar = async (userId, title, descripcion, etiquetas) => {
+// Colección leer datos para despues publicar Post
+export const publicar = async (userId, descripcion, etiquetas) => {
   try {
-    const docRef = await addDoc(collection(db, "post"), {
-      id: auth.currentUser.uid,
-      title,
+    const docRef = await addDoc(collection(db, 'post'), {
+      id: userId,
       descripcion,
       etiquetas,
     });
-    console.log("Document written with ID: ", docRef.id);
+    console.log('Document written with ID: ', docRef.id);
   } catch (e) {
-    console.error("Error adding document: ", e);
+    console.error('Error adding document: ', e);
   }
 };
 
@@ -67,9 +66,9 @@ export const newRegister = (email, password, userName, age) => {
       userData(auth.currentUser.uid, userName, age);
       verificar();
       alert(
-        "Se ha enviado un correo electrónico de verificación. Por favor revisa tu bandeja de entrada o spam."
+        'Se ha enviado un correo electrónico de verificación. Por favor revisa tu bandeja de entrada o spam.',
       );
-      window.location.hash = "#/login";
+      window.location.hash = '#/login';
       return user;
     })
     .catch((error) => {
@@ -80,38 +79,38 @@ export const newRegister = (email, password, userName, age) => {
       console.log(errorCode + errorMessage);
       // return errorCode + errorMessage;
       // Llamada constantes
-      const missingEmail = document.getElementById("missinEmail");
-      const loginNulo = document.getElementById("loginEmailNull");
-      const emailInUse = document.getElementById("registerEmailInUse");
-      const weakPassword = document.getElementById("registerWeakPassword");
-      const missingPassword = document.getElementById("missinPassword");
+      const missingEmail = document.getElementById('missinEmail');
+      const loginNulo = document.getElementById('loginEmailNull');
+      const emailInUse = document.getElementById('registerEmailInUse');
+      const weakPassword = document.getElementById('registerWeakPassword');
+      const missingPassword = document.getElementById('missinPassword');
 
       // Error campo correo vacio:
-      if (errorCode == "auth/missing-email") {
-        missingEmail.style.display = "block";
-        emailInUse.style.display = "none";
-        loginNulo.style.display = "none";
+      if (errorCode == 'auth/missing-email') {
+        missingEmail.style.display = 'block';
+        emailInUse.style.display = 'none';
+        loginNulo.style.display = 'none';
       }
-      if (errorCode == "auth/invalid-email") {
-        loginNulo.style.display = "block";
-        missingEmail.style.display = "none";
-        emailInUse.style.display = "none";
+      if (errorCode == 'auth/invalid-email') {
+        loginNulo.style.display = 'block';
+        missingEmail.style.display = 'none';
+        emailInUse.style.display = 'none';
       }
       // Mensaje correo en uso
-      if (errorCode == "auth/email-already-in-use") {
-        emailInUse.style.display = "block";
-        missingEmail.style.display = "none";
-        loginNulo.style.display = "none";
+      if (errorCode == 'auth/email-already-in-use') {
+        emailInUse.style.display = 'block';
+        missingEmail.style.display = 'none';
+        loginNulo.style.display = 'none';
       }
       // Mensaje contraseña demasiado debil (Menos de 6 caracteres)
-      if (errorCode == "auth/weak-password") {
-        weakPassword.style.display = "block";
-        missingPassword.style.display = "none";
+      if (errorCode == 'auth/weak-password') {
+        weakPassword.style.display = 'block';
+        missingPassword.style.display = 'none';
       }
       // Error campo de contraseña vacio
-      if (errorCode == "auth/internal-error") {
-        missingPassword.style.display = "block";
-        weakPassword.style.display = "none";
+      if (errorCode == 'auth/internal-error') {
+        missingPassword.style.display = 'block';
+        weakPassword.style.display = 'none';
       }
     });
   return createUserWithEmailAndPassword;
@@ -128,7 +127,7 @@ export const loginGoogle = () => {
       // The signed-in user info.
       const user = result.user;
       // ...
-      window.location.hash = "#/home";
+      window.location.hash = '#/home';
     })
     .catch((error) => {
       // Handle Errors here.
@@ -149,9 +148,9 @@ export const loginEmail = (email, password) => {
       // Signed in
       const user = userCredential.user;
       // ...
-      alert("ingresaste");
+      alert('ingresaste');
       // LLeva al home
-      window.location.hash = "#/home";
+      window.location.hash = '#/home';
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -159,29 +158,29 @@ export const loginEmail = (email, password) => {
       console.log(errorCode + errorMessage);
 
       // Constantes
-      const errorContraseña = document.getElementById("loginContrañaInvalida");
-      const loginNulo = document.getElementById("loginEmailNull");
-      const errorEmail = document.getElementById("loginEmailInvalido");
-      const passwordNull = document.getElementById("loginContraseñaVacia");
+      const errorContraseña = document.getElementById('loginContrañaInvalida');
+      const loginNulo = document.getElementById('loginEmailNull');
+      const errorEmail = document.getElementById('loginEmailInvalido');
+      const passwordNull = document.getElementById('loginContraseñaVacia');
       // Contraseña incorrecta
-      if (errorCode == "auth/wrong-password") {
-        errorContraseña.style.display = "block";
-        passwordNull.style.display = "none";
+      if (errorCode == 'auth/wrong-password') {
+        errorContraseña.style.display = 'block';
+        passwordNull.style.display = 'none';
       }
       // Ingresar correo valido
-      if (errorCode == "auth/invalid-email") {
-        loginNulo.style.display = "block";
-        errorEmail.style.display = "none";
+      if (errorCode == 'auth/invalid-email') {
+        loginNulo.style.display = 'block';
+        errorEmail.style.display = 'none';
       }
       // Email invalido
-      if (errorCode == "auth/user-not-found") {
-        errorEmail.style.display = "block";
-        loginNulo.style.display = "none";
+      if (errorCode == 'auth/user-not-found') {
+        errorEmail.style.display = 'block';
+        loginNulo.style.display = 'none';
       }
       // Campo contraseña vacio:
-      if (errorCode == "auth/internal-error") {
-        passwordNull.style.display = "block";
-        errorContraseña.style.display = "none";
+      if (errorCode == 'auth/internal-error') {
+        passwordNull.style.display = 'block';
+        errorContraseña.style.display = 'none';
       }
     });
 };
@@ -189,7 +188,7 @@ export const loginEmail = (email, password) => {
 // Función que envía un correo de verificación al registrarse con email y contraseña.
 const verificar = () => {
   sendEmailVerification(auth.currentUser).then(() => {
-    console.log("Mail enviado");
+    console.log('Mail enviado');
     // Email verification sent!
     // ...
   });
@@ -198,8 +197,8 @@ const verificar = () => {
 export const cerrarSesion = () => {
   signOut(auth)
     .then(() => {
-      alert("Chaito");
-      window.location.hash = "#/login";
+      alert('Chaito');
+      window.location.hash = '#/login';
       // Sign-out successful.
     })
     .catch((error) => {
@@ -212,7 +211,7 @@ export const observador = () => {
   onAuthStateChanged(auth, (user) => {
     if (user) {
       console.log(user);
-      window.location.hash = "#/home";
+      window.location.hash = '#/home';
       routes(window.location.hash);
 
       // User is signed in, see docs for a list of available properties
