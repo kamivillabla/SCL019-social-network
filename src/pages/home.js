696f3c65f0900dcfb4d38f9e4d1b5682db1727be
karen <kdescobar21@gmail.com>
2022-03-09 13:04:33 -0300

    buscador

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,16 +6,26 @@ export const home = () => {
   const containerHome = document.createElement('section');
   containerHome.className = 'containerRootHome';
   const postHtml = `<header class="header">
+ <div class="LogoAndTitle">
   <img class="home__logoImg" src="assets/img/logoMartini.png" alt="Logo buena muchacha">
   <h1 class="home__title">BUENAS MUCHACHAS</h1>
-  <i class="fa-solid fa-door-open iconsColor iconsColor__header" id="cerrarSesion"></i>
+ </div>
+ <div class="buscar">
+  <input type="text" placeholder="Buscar" required>
+  <div class ="btn">
+    <i class="fa-solid fa-magnifying-glass iconsColor"></i>
+  </div>
+</div>
+  <div class="icons-door">
+    <i class="fa-solid fa-door-open iconsColor iconsColor__header" id="cerrarSesion"></i>
+  </div>
 </header>
 <!-- Perfil de usuaria -->
 <article class="perfilDeUser">
   <div class="container-Datos">
     <spam class="barra"></spam>
     <img class="home__imgUsuaria" src="../assets/css/imgUsuarie.png" alt="Imagen de perfil">
-    <h3>Nombre de usuaria</h3>
+    <h3>usuarie</h3>
     <p>edad</p>
     <hr class="hr-perfil">
     <div class="likeAndComment">
@@ -26,16 +36,19 @@ export const home = () => {
   </div>
 </article>
  <!-- Publicar -->
-<main class="main-home">
+ <main class="main-home">
   <div class="home__publicar">
     <div class="containerImgUsuaria">
       <img class="home__imgUsuaria" src="../assets/css/imgUsuarie.png" alt="Imagen usuarie">
     </div>
     <div class="home__inputPublicar">
-      <h3 class="nombreUsuarie"> Nombre usuarie</h3>
-      <input type="text" class="home__description" id="descripcion" placeholder="¿Qué nos queires contar, buena muchacha?">
-      <input type="text" class="home__etiquetas" id="etiquetas" placeholder="Etiquetas">
-      <button class="buttonPublicar" id="publicar">Publicar </button> 
+      <h3 class="nombreUsuarie-perfil"> Usuarie</h3>
+      <form class="form-inpus-home" id="reset">
+        <input type="text" class="home__titulo" id="titulo" placeholder="Título">
+       <!--  <input type="text" class="home__description" id="descripcion" placeholder="¿Qué nos queires contar?"> -->
+       <textarea class="home__description" id="descripcion" name="text-area" rows="1" cols="50" placeholder="¿Qué nos queires contar?"></textarea>
+      </form>
+     <button class="buttonPublicar" id="publicar">Publicar</button> 
     </div>
   </div>
    <!-- Publicaciones -->
@@ -47,7 +60,7 @@ export const home = () => {
 <!-- Barra  -->
 <div class="nav">
   <i class="fa-solid fa-user iconsColor"></i>
-  <i class="fa-solid fa-filter iconsColor"></i>
+ 
   <div class="buscar">
   <input type="text" placeholder="Buscar" required>
   <div class ="btn">
@@ -60,11 +73,19 @@ export const home = () => {
 
   containerHome.innerHTML = postHtml;
 
+  /* Guardamos los datos para agregarlos a la data */
   const post = containerHome.querySelector('#publicar');
   post.addEventListener('click', () => {
     const descripcion = containerHome.querySelector('#descripcion').value;
-    const etiquetas = containerHome.querySelector('#etiquetas').value;
-    addData(descripcion, etiquetas);
+    const titulo = containerHome.querySelector('#titulo').value;
+    /* Si los inputs de descripción y titulo estan vacios no se puede publicar */
+    if (descripcion === '' || titulo === '') {
+      alert('Contenido vacio. No puedes publicar');
+    } else {
+      addData(descripcion, titulo);
+      /* Resetea el form cada vez que se le da click al botton para publicar */
+      containerHome.querySelector('#reset').reset();
+    }
   });
   showPost();
 
