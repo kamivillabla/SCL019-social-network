e05fbfd11f7cadc926a0c183ad338374875b68ca
Kamila <kvillablancabaez@gmail.com>
2022-03-09 00:18:13 -0300

    Solo los usuarios del post lo pueden borrar

--- src/component/post.js
+++ src/component/post.js
@@ -5,27 +5,47 @@ import { readData, deletedDataPost, auth, likes } from '../lib/firebase.js';
 export const newPost = (posts) => {
   const containerPost = document.getElementById('postContainer');
   containerPost.innerHTML = '';
-  const postContent = (postData) => {
-    const templatePost = `<div class="home__publicaciones">
+  const postContent = (data) => {
+    let themePost = `<div class="home__publicaciones">
     <div class="containerImgUsuaria">
-      <img class="home__imgUsuaria" src="${postData.element.data.photo}" alt="Imagen usuarie">
+      <img class="home__imgUsuaria" src="${data.element.data.photo}" alt="Imagen usuarie">
     </div>
     <div class="home__inputPublicar">
      <div class="containerNameUsarieYdelete">
-        <h3 class="nombreUsuarie">${postData.element.data.name}</h3>
-       <button class="icons-delete delete" value=${postData.element.id} id="button"><i class="fa-solid fa-trash-arrow-up"></i></button>
-      </div>
-      <p class="publicarDescripcion"> ${postData.element.data.titulos}.</p>
-      <p class="publicarDescripcion"> ${postData.element.data.description}.</p>
-      <hr>
-        <div class="likeAndComment">
-          <button class="buttonLike" value=${postData.element.id}> <span class="counterLike">${postData.element.data.likesCounter}</span> <i class="fa-solid fa-heart"></i></button>
-         <span class="counterLike">7</span>
-          <i class="fa-solid fa-comment"></i>
-        </div>
+        <h3 class="nombreUsuarie">${data.element.data.name}</h3>
+       
+      `;
+
+  if (data.element.data.userId === auth.currentUser.uid) {
+     themePost += `<button class="icons-delete delete" value=${data.element.id} id="button"><i class="fa-solid fa-trash-arrow-up"></i></button> 
     </div>
-</div>`;
-    containerPost.innerHTML += templatePost;
+        <p class="publicarDescripcion"> ${data.element.data.titulos}.</p>
+        <p class="publicarDescripcion"> ${data.element.data.description}.</p>
+        <hr>
+          <div class="likeAndComment">
+            <button class="buttonLike" value=${data.element.id}> <span class="counterLike">${data.element.data.likesCounter}</span> <i class="fa-solid fa-heart"></i></button>
+          <span class="counterLike">7</span>
+            <i class="fa-solid fa-comment"></i>
+          </div>
+      </div>
+  </div>
+    `;
+  }else {
+     themePost += `</div>
+    <p class="publicarDescripcion"> ${data.element.data.titulos}.</p>
+    <p class="publicarDescripcion"> ${data.element.data.description}.</p>
+    <hr>
+      <div class="likeAndComment">
+        <button class="buttonLike" value=${data.element.id}> <span class="counterLike">${data.element.data.likesCounter}</span> <i class="fa-solid fa-heart"></i></button>
+      <span class="counterLike">7</span>
+        <i class="fa-solid fa-comment"></i>
+      </div>
+  </div>
+  </div>
+    `;
+ 
+}
+    containerPost.innerHTML += themePost;
   };
 
   /* Recorre los post y los despliega todos en la pantalla */
